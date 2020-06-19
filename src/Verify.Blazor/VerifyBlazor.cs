f8e15b2b15654fbd13318a85f8b2bb252ec8b51f
SimonCropp <simon.cropp@gmail.com>
2020-06-19 14:34:24 +1000

    .

--- src/Verify.Blazor/VerifyBlazor.cs
+++ src/Verify.Blazor/VerifyBlazor.cs
@@ -1,5 +1,6 @@
 ﻿using System.IO;
 using System.Reflection;
+using System.Threading.Tasks;
 using Microsoft.AspNetCore.Components;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Logging.Abstractions;
@@ -9,40 +10,41 @@ namespace VerifyTests
 {
     public static class VerifyBlazor
     {
-        static MethodInfo stateHasChanged = typeof(ComponentBase).GetMethod("StateHasChanged",BindingFlags.Instance|BindingFlags.NonPublic);
+        static MethodInfo stateHasChanged = typeof(ComponentBase).GetMethod("StateHasChanged", BindingFlags.Instance | BindingFlags.NonPublic);
+
         public static void Initialize()
         {
             VerifierSettings.RegisterFileConverter<Render>(
                 "html",
-                async (target, settings) =>
+                async (target, settings) => await RenderToResult(target));
+        }
+
+        static async Task<ConversionResult> RenderToResult(Render target)
+        {
+            await using var provider = GetProvider(target);
+            var loggerFactory = target.LoggerFactory ?? NullLoggerFactory.Instance;
+            var renderer = new TestRenderer(provider, loggerFactory);
+
+            var root = new ContainerComponent(renderer);
+            var parameters = target.ParameterView ?? ParameterView.Empty;
+
+            var type = target.Type;
+            await root.RenderComponentUnderTest(type, parameters);
+            var (componentId, component) = root.FindComponentUnderTest();
+            if (target.BeforeRender != null)
             {
-                await using var provider = GetProvider(target);
-                var loggerFactory = target.LoggerFactory ?? NullLoggerFactory.Instance;
-                var renderer = new TestRenderer(provider, loggerFactory);
-
-                var root = new ContainerComponent(renderer);
-                var parameters = target.ParameterView?? ParameterView.Empty;
-
-                var type = target.Type;
+                target.BeforeRender(component);
+                await root.renderHandle.Dispatcher.InvokeAsync(() => { stateHasChanged.Invoke(component, null); });
                 await root.RenderComponentUnderTest(type, parameters);
-                var (componentId, component) = root.FindComponentUnderTest();
-                if (target.BeforeRender != null)
-                {
-                    target.BeforeRender(component);
-                    await root.renderHandle.Dispatcher.InvokeAsync(() =>
-                    {
-                        stateHasChanged.Invoke(component, null);
-                    });
-                    await root.RenderComponentUnderTest(type, parameters);
-                }
-                var html = Htmlizer.GetHtml(renderer, componentId);
+            }
 
-                var stream = new MemoryStream();
-                await using var writer = stream.BuildLeaveOpenWriter();
-                writer.WriteLine(html);
+            var html = Htmlizer.GetHtml(renderer, componentId);
 
-                return new ConversionResult(null, stream);
-            });
+            var stream = new MemoryStream();
+            await using var writer = stream.BuildLeaveOpenWriter();
+            writer.WriteLine(html);
+
+            return new ConversionResult(null, stream);
         }
 
         static ServiceProvider GetProvider(Render target)
@@ -51,6 +53,7 @@ namespace VerifyTests
             {
                 return target.Provider;
             }
+
             var services = new ServiceCollection();
             return services.BuildServiceProvider();
         }
