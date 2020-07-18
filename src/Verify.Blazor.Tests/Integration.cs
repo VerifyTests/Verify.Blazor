d5cef07bc867c265677c06566315beddcf92669c
Simon Cropp <simon.cropp@gmail.com>
2020-07-18 10:28:22 +1000

    .

--- src/Verify.Blazor.Tests/Integration.cs
+++ src/Verify.Blazor.Tests/Integration.cs
@@ -3,7 +3,9 @@ using System.Diagnostics;
 using System.Drawing;
 using System.IO;
 using System.Threading.Tasks;
+using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
 using VerifyTests;
 using VerifyXunit;
 using Xunit;
@@ -17,14 +19,22 @@ public class Integration
         VerifySelenium.Enable();
 
         var binPath = AppDomain.CurrentDomain.BaseDirectory!.Replace("Verify.Blazor.Tests", "BlazorApp");
-        var appDll = Path.GetFullPath(Path.Combine(binPath, "BlazorApp.dll"));
-        using var process = Process.Start("dotnet", appDll);
+        var projectDirectory = Path.GetFullPath(Path.Combine(binPath, "../../"));
+        var startInfo = new ProcessStartInfo("dotnet", "run")
+        {
+            WorkingDirectory = projectDirectory
+        };
+        using var process = Process.Start(startInfo);
         var options = new ChromeOptions();
         options.AddArgument("--no-sandbox");
         options.AddArgument("--headless");
         var driver = new ChromeDriver(options);
         driver.Manage().Window.Size = new Size(1024, 768);
-        driver.Navigate().GoToUrl("http://localhost:50419");
+        driver.Navigate().GoToUrl("http://localhost:5000");
+
+        var wait = new WebDriverWait(driver, TimeSpan.FromSeconds(5));
+        wait.Until(drv => drv.FindElement(By.ClassName("main")));
+
         await Verifier.Verify(driver);
         process.Kill();
     }
