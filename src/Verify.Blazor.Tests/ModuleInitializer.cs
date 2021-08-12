2575fbbb6cc3bb289db737be1cce37cff38f777a
Simon Cropp <simon.cropp@gmail.com>
2021-08-12 22:56:03 +1000

    Update ModuleInitializer.cs

--- src/Verify.Blazor.Tests/ModuleInitializer.cs
+++ src/Verify.Blazor.Tests/ModuleInitializer.cs
@@ -8,10 +8,9 @@ public static class ModuleInitializer
     [ModuleInitializer]
     public static void Initialize()
     {
-        // remove some noise from the html snapshot
-
         #region scrubbers
-
+        
+        // remove some noise from the html snapshot
         VerifierSettings.ScrubEmptyLines();
         VerifierSettings.ScrubLinesWithReplace(s => s.Replace("<!--!-->", ""));
         HtmlPrettyPrint.All();
