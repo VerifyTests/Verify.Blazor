a9251e59667e1d794f5ea6365ed23b680a83133e
Simon Cropp <simon.cropp@gmail.com>
2022-03-17 21:41:59 +1100

    Update VerifyBunit.cs

--- src/Verify.Bunit/VerifyBunit.cs
+++ src/Verify.Bunit/VerifyBunit.cs
@@ -19,16 +19,16 @@ public static class VerifyBunit
     }
 
     /// <summary>
-    /// Wait until the provided <paramref name="statePredicate"/> action returns true,
+    /// Wait until the provided <paramref name="predicate"/> action returns true,
     /// or the <paramref name="timeout"/> is reached (default is one second).
     ///
-    /// The <paramref name="statePredicate"/> is evaluated initially, and then each time
-    /// the <paramref name="renderedFragment"/> renders.
+    /// The <paramref name="predicate"/> is evaluated initially, and then each time
+    /// the <paramref name="fragment"/> renders.
     /// </summary>
-    /// <param name="renderedFragment">The render fragment or component to attempt to verify state against.</param>
-    /// <param name="statePredicate">The predicate to invoke after each render, which must returns <c>true</c> when the desired state has been reached.</param>
+    /// <param name="fragment">The render fragment or component to attempt to verify state against.</param>
+    /// <param name="predicate">The predicate to invoke after each render, which must returns <c>true</c> when the desired state has been reached.</param>
     /// <param name="timeout">The maximum time to wait for the desired state.</param>
-    /// <exception cref="WaitForFailedException">Thrown if the <paramref name="statePredicate"/> throw an exception during invocation, or if the timeout has been reached. See the inner exception for details.</exception>
+    /// <exception cref="WaitForFailedException">Thrown if the <paramref name="predicate"/> throw an exception during invocation, or if the timeout has been reached. See the inner exception for details.</exception>
     public static async Task WaitForStateAsync(this IRenderedFragmentBase fragment, Func<bool> predicate, TimeSpan? timeout = null)
     {
         using var waiter = new WaitForStateHelper(fragment, predicate, timeout);
