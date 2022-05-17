66a90d361d59b6b402483c3b72bb8b38a9c7489c
Simon Cropp <simon.cropp@gmail.com>
2022-05-17 22:15:37 +1000

    refs

--- src/Verify.Bunit/VerifyBunit.cs
+++ src/Verify.Bunit/VerifyBunit.cs
@@ -8,14 +8,8 @@ public static class VerifyBunit
     public static void Initialize()
     {
         VerifierSettings.RegisterFileConverter<IRenderedFragment>(FragmentToStream.Convert);
-        VerifierSettings.ModifySerialization(settings =>
-        {
-            settings.AddExtraSettings(serializerSettings =>
-            {
-                var converters = serializerSettings.Converters;
-                converters.Add(new RenderedFragmentConverter());
-            });
-        });
+        VerifierSettings.AddExtraSettings(
+            _ => _.Converters.Add(new RenderedFragmentConverter()));
     }
 
     /// <summary>
